0d3c8b1b9944a7fd9662f3aaa832e559f971f2dd
samesh97 <Alahakoon123@>
2020-08-07 13:13:36 +0530

    Added Logo and Title of the window

--- SPM_Project/src/application/Main.java
+++ SPM_Project/src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
@@ -30,6 +31,9 @@ public class Main extends Application{
 			 primaryStage.setMinWidth(1100);
 			 primaryStage.setMaxHeight(850);
 			 primaryStage.setMaxWidth(1100);
+			 
+			 primaryStage.setTitle("SLIIT Time Table Management");
+			 primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("../media/SLIIT_Logo_Crest.png")));
 			 primaryStage.show();
 		}
 		catch(Exception e)
