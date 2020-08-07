a1c5223e0dae14da1c65f0bcf90346c8a89b2b7a
samesh97 <Alahakoon123@>
2020-08-07 10:36:17 +0530

    Created Main Fxml

--- SPM_Project/src/application/Main.java
+++ SPM_Project/src/application/Main.java
@@ -1,8 +1,10 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -12,11 +14,19 @@ public class Main extends Application{
 	{
 		try
 		{
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root,1000,700);
+//			scene.getStylesheets().add(getClass().getResource("Main.fxml").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+			
+			
+			//load main fxml file
+			 FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/Main.fxml"));
+			 AnchorPane root = (AnchorPane) loader.load();
+			 Scene scene = new Scene(root);
+			 primaryStage.setScene(scene);
+			 primaryStage.show();
 		}
 		catch(Exception e)
 		{
