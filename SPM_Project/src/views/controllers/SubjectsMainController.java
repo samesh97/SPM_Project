e733d083a19f0a0f313378a3e2bea7f841b8cb2a
Acer <pamoshakishani@gmail.com>
2020-08-12 02:18:51 +0530

    designed interface of Subjects

--- SPM_Project/src/views/controllers/SubjectsMainController.java
+++ SPM_Project/src/views/controllers/SubjectsMainController.java
@@ -9,8 +9,20 @@ public class SubjectsMainController {
 	
 	@FXML
 	private Button btn_button1;
+	@FXML
+	private Button btn_ViewAllSubjects1;
+	@FXML
+	private Button btn_Add;
 
 	public void onButton1Selected(ActionEvent event) {
 		System.out.println("Button 1 selected");
+	}
+	
+	public void onViewAllSubjectsClicked(ActionEvent event) {
+		System.out.println("View All Subjects clicked");
+	}
+	
+	public void onAddClicked(ActionEvent event) {
+		System.out.println("Add Subjects clicked");
 	}
 }
