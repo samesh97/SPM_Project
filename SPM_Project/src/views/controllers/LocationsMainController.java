54bfc9a310c02393c19eb1ed7157d4322af3a203
ABC <ABC@DESKTOP-DA51JO9>
2020-08-20 11:45:44 +0530

    Locations main controller file

---
+++ SPM_Project/src/views/controllers/LocationsMainController.java
@@ -0,0 +1,19 @@
+package views.controllers;
+
+import javafx.event.ActionEvent;
+
+public class LocationsMainController {
+	
+	public void onAddNewLocationButtonClicked(ActionEvent event) {
+		System.out.println("Hello From Add New Location");
+	}
+
+	public void onDeleteAllLocationsButtonClicked(ActionEvent event) {
+		System.out.println("Hello From Delete All Locations");
+	}
+	
+	public void onViewAllLocationsButtonClicked(ActionEvent event) {
+		System.out.println("Hello From View All Locations");
+	}
+
+}
