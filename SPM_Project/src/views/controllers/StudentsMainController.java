4dccf0d279a82b1385be910ab37ae15021bb0348
chath <chath@192.168.1.37>
2020-09-06 18:00:13 +0530

    add student

--- SPM_Project/src/views/controllers/StudentsMainController.java
+++ SPM_Project/src/views/controllers/StudentsMainController.java
@@ -1,49 +1,117 @@
 package views.controllers;
 
+import database.DatabaseHandler_Lecturers;
+import database.DatabaseHandler_Students;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 public class StudentsMainController
 {
+	
 	@FXML
-	private Button btn_ViewAllStudent;
+	private TextField yearSemText;
+	@FXML
+	private TextField programText;
+	@FXML
+	private TextField GroupNoText;
+	@FXML
+	private TextField SubNoText;
+	@FXML
+	private TextField groupIDText;
+	@FXML
+	private TextField subIDText;
 	
+	@FXML
+	private Button addStudentButton;
 	
-	
-	public void onSampleButtonClicked(ActionEvent event)
-	{
-		System.out.println("Hello From Student");
-		Scene scene = btn_ViewAllStudent.getScene();
-		AnchorPane pane = (AnchorPane) scene.lookup("#controllerPane");
-		changeCenterContent(pane,"../StudentsView.fxml");
-	}
-	public void changeCenterContent(AnchorPane controllerPane,String fxmlFileName)
-	{
+	public void AddNewStudentClicked(ActionEvent event) {
 		
-		try
-		{
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName));
-			Node _node = loader.load();
-			AnchorPane.setTopAnchor(_node, 0.0);
-			AnchorPane.setRightAnchor(_node, 0.0);
-			AnchorPane.setLeftAnchor(_node, 0.0);
-			AnchorPane.setBottomAnchor(_node, 0.0);
-			// container child clear
-			controllerPane.getChildren().clear();
+		String yearSem = yearSemText.getText();
+		String program = programText.getText();
+		String gNo = GroupNoText.getText();
+		String subNo = SubNoText.getText();
+		String gId = groupIDText.getText();
+		String subId = subIDText.getText();
+		
+		
+		try {
+		    boolean result= DatabaseHandler_Students.addStudents(yearSem, program, gNo, subNo, gId, subId);
+			if(result== true) {
+				showAlert("Successfully added");
+			}
+			else {
+				showAlert("Unsuccessful");
+			}
+	
+	}catch(Exception e) {
+    	showAlert("Please enter details correctly");
+    }
+	
+	}
 
-			// new container add
-			controllerPane.getChildren().add(_node);
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		
-		
+	
+	public void showAlert(String message)
+	{
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setContentText(message);
+		alert.show();
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	@FXML
+//	private Button btn_ViewAllStudent;
+//	
+//	
+//	
+//	public void onSampleButtonClicked(ActionEvent event)
+//	{
+//		System.out.println("Hello From Student");
+//		Scene scene = btn_ViewAllStudent.getScene();
+//		AnchorPane pane = (AnchorPane) scene.lookup("#controllerPane");
+//		changeCenterContent(pane,"../StudentsView.fxml");
+//	}
+//	public void changeCenterContent(AnchorPane controllerPane,String fxmlFileName)
+//	{
+//		
+//		try
+//		{
+//			FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName));
+//			Node _node = loader.load();
+//			AnchorPane.setTopAnchor(_node, 0.0);
+//			AnchorPane.setRightAnchor(_node, 0.0);
+//			AnchorPane.setLeftAnchor(_node, 0.0);
+//			AnchorPane.setBottomAnchor(_node, 0.0);
+//			// container child clear
+//			controllerPane.getChildren().clear();
+//
+//			// new container add
+//			controllerPane.getChildren().add(_node);
+//		}
+//		catch(Exception e)
+//		{
+//			e.printStackTrace();
+//		}
+//		
+//		
+//	}
 }
