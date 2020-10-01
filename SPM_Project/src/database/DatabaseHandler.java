258e178bd00f23224cb654580f997767ee51a315
samesh97 <Alahakoon123@>
2020-10-01 15:14:00 +0530

    New Database was added

--- SPM_Project/src/database/DatabaseHandler.java
+++ SPM_Project/src/database/DatabaseHandler.java
@@ -11,15 +11,20 @@ import listeners.OnTaskCompleteListener;
 public class DatabaseHandler
 {
 	//DB4Free
-	private static final String USER_NAME = "error_404";
-	private static final String PASSWORD = "error_404";
+//	private static final String USER_NAME = "error_404";
+//	private static final String PASSWORD = "error_404";
 	
 	//FREE SQL HOSTING
-//	private static final String USER_NAME = "sql12361005";
-//	private static final String PASSWORD = "IwIYLZd2HX";
+	private static final String USER_NAME = "sql12368186";
+	private static final String PASSWORD = "CYPPc3tUHC";
+	
+	//Local DB
+//	private static final String USER_NAME = "root";
+//	private static final String PASSWORD = "12345";
 	
 	public static Connection conn = null;
 	
+
 	public static void makeConnection(OnTaskCompleteListener listener)
 	{
 		
@@ -27,11 +32,14 @@ public class DatabaseHandler
 		{
 			Class.forName("com.mysql.jdbc.Driver");
 			
+			//Local DB
+//			conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/spm_timetable_db",USER_NAME,PASSWORD);
+			
 			//DB4Free
-			conn = (Connection) DriverManager.getConnection("jdbc:mysql://db4free.net:3306/spm_timetable_db",USER_NAME,PASSWORD);
+//			conn = (Connection) DriverManager.getConnection("jdbc:mysql://db4free.net:3306/spm_timetable_db",USER_NAME,PASSWORD);
 			
 			//FREE SQL HOSTING
-//			conn = (Connection) DriverManager.getConnection("jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12361005",USER_NAME,PASSWORD);
+			conn = (Connection) DriverManager.getConnection("jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12368186",USER_NAME,PASSWORD);
 			listener.onFinished(true);
 			System.out.println("Successfully Connected to the Database");
 			
@@ -45,4 +53,5 @@ public class DatabaseHandler
 		
 		
 	}
+
 }
