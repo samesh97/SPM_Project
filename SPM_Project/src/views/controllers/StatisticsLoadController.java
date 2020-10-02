982f668ed2119b2bda153014dc2b1b55309a1da8
AILI <ailichandrasekara@gmail.com>
2020-10-02 10:51:44 +0530

    StatisticsLoadController done

---
+++ SPM_Project/src/views/controllers/StatisticsLoadController.java
@@ -0,0 +1,27 @@
+package views.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class StatisticsLoadController implements Initializable{
+
+	public void onLoadStatisticsButtonClicked(){
+		System.out.println("Lecturer Load statistics button clicked");
+	}
+
+	public void onLoadStatisticsStudentButtonClicked(){
+		System.out.println("Student Load statistics button clicked");
+	}
+	
+	public void onLoadStatisticsSubjectButtonClicked(){
+		System.out.println("Student Load statistics button clicked");
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
